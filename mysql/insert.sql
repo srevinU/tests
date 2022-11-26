f8c6ab1a3280524f105a5b9195ccd524f550ec66
Cedric Segura <cedricsegurafr@gmail.com>
2022-11-26 15:39:15 +0100

    modif

--- mysql/insert.sql
+++ mysql/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO persons (PersonID, LastName, FirstName, Address, City)
-VALUES (123, "SEGURA", "CEDRIC", "10 rue du capitol", "Toulouse");
+VALUES (123, "SEGURA", "CEDRIC", "10 rue du toto", "Paris");
 
 INSERT INTO persons_referential (PersonID, LastName, FirstName, Address, City)
-VALUES (123, "SHIN", "SAUYEUN", "10 rue du capitol", "Toulouse");
+VALUES (123, "SHIN", "SAUYEUN", "10 rue du toto", "Paris");
